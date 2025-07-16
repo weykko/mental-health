25a0c5c3dffc5673a318d7fa5eb4d7e29241c46e
aleksandr <a.d.beznutrov@urfu.me>
2025-07-16 18:50:08 +0500

    created datasets.py

---
+++ utils/datasets.py
@@ -0,0 +1,34 @@
+import torch
+from torch.utils.data import Dataset
+
+
+class DepressionDataset(Dataset):
+    def __init__(self, texts, labels, tokenizer, max_length):
+        self.texts = texts
+        self.labels = labels
+        self.tokenizer = tokenizer
+        self.max_length = max_length
+
+    def __len__(self):
+        return len(self.texts)
+
+    def __getitem__(self, idx):
+        text = str(self.texts.iloc[idx])
+        label = self.labels.iloc[idx]
+
+        encoding = self.tokenizer.encode_plus(
+            text,
+            add_special_tokens=True,
+            max_length=self.max_length,
+            return_token_type_ids=False,
+            padding='max_length',
+            truncation=True,
+            return_attention_mask=True,
+            return_tensors='pt'
+        )
+
+        return {
+            'input_ids': encoding['input_ids'].flatten(),
+            'attention_mask': encoding['attention_mask'].flatten(),
+            'label': torch.tensor(label, dtype=torch.long)
+        }
